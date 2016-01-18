7c75b3c60ee759746651f1cc071dc32d946180f5
Wiiz- <wiiz@live.co.uk>
2016-01-18 20:00:17 +0000

    Sorted

--- src/decomp/main/DecompMain.java
+++ src/decomp/main/DecompMain.java
@@ -1,16 +1,31 @@
 package decomp.main;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class DecompMain
 {
 
     public DecompMain()
     {
-        
+        JFrame frame = new JFrame("Decompiler");
+        JPanel panel = (JPanel) frame.getContentPane();
+        frame.setSize(new Dimension(600, 400));
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setLayout(null);
+
+        JButton button = new JButton("OK");
+        button.setBounds(100, 100, 100, 25);
+        panel.add(button);
+
+        frame.setVisible(true);
     }
 
     public static void main(String argsp[])
     {
-
+        new DecompMain();
     }
 
 }
