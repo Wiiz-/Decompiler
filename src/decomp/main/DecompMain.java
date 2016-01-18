51efaac778d623ebadea0237c71e5dd1abf63d9c
Wiiz- <wiiz@live.co.uk>
2016-01-18 20:18:22 +0000

    Adjusted Positions and added text area

--- src/decomp/main/DecompMain.java
+++ src/decomp/main/DecompMain.java
@@ -16,9 +16,15 @@ public class DecompMain
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel.setLayout(null);
 
-        JButton button = new JButton("OK");
-        button.setBounds(100, 100, 100, 25);
+        JButton button = new JButton("Decompile");
+        button.setBounds(485, 365, 110, 25);
+
+        JTextArea display = new JTextArea();
+        display.setBounds(10, 10, 470, 380);
+        display.setEditable(false);
+
         panel.add(button);
+        panel.add(display);
 
         frame.setVisible(true);
     }
